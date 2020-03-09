0cffc6c6ff8cdf9881a09a6f99e7ff1f8b02147f
Evgen904 <evgen904@gmail.com>
2020-03-10 00:49:22 +0300

    fix heroku 404 page

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
-const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const express = require('express')
+const compression = require('compression')
+const history = require('connect-history-api-fallback')
+
+const staticMiddleWare = express.static('dist')
+
+const app = express()
+app.use(compression())
+app.use(staticMiddleWare)
+app.use(history())
+app.use(staticMiddleWare)
+
+app.use(express.static('dist'))
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
+})
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  next()
+})
+
+app.set('port', (process.env.PORT || 3333))
+app.listen(app.get('port'), () =>
+    console.log('Server @', app.get('port')))
