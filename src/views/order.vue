ff0e9b0921f9baed35f309964ce5c48429d75284
Evgen904 <evgen904@gmail.com>
2020-03-15 22:55:57 +0300

    add order page

--- src/views/order.vue
+++ src/views/order.vue
@@ -1,23 +1,153 @@
 <template>
   <div>
-    <Head title="Заказ" />
-    Order
-    <br>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda at consequatur cupiditate delectus deleniti ducimus ea eaque eligendi exercitationem fugiat hic ipsa, itaque iusto magni nemo nihil nulla odit omnis quae repellat sequi soluta tempora tenetur ullam ut veritatis voluptatum! Ab accusamus accusantium animi aut doloremque dolores earum enim et fuga magni modi necessitatibus obcaecati omnis, repudiandae, rerum saepe similique totam velit, veniam vero? Animi aspernatur atque autem beatae debitis dolore dolorem, ducimus, eius enim eos esse fugiat magnam magni necessitatibus, nisi omnis qui. Accusamus adipisci architecto aut consequatur cum dicta error, et eveniet illum nobis, odio quisquam, quod repellat? A accusamus alias aliquam, architecto aspernatur dolor enim eveniet ex explicabo facilis fugiat impedit in iusto laborum magni molestias natus nostrum omnis qui quo repellendus sint sunt suscipit tempore ullam ut voluptates. Esse iste molestias numquam provident reprehenderit. A commodi consectetur cumque dolores excepturi fugiat ipsum natus necessitatibus nisi quia? Accusamus alias aliquid doloremque, dolorum, error ex fuga incidunt iste laborum placeat quibusdam sapiente ullam, ut voluptate voluptatem. Accusamus accusantium aliquid aspernatur beatae consectetur consequuntur cupiditate debitis distinctio dolorum earum eveniet fuga fugiat illo, in molestiae molestias obcaecati odit officiis perferendis placeat quasi qui quia recusandae reprehenderit repudiandae similique sint tempora tempore unde velit veniam veritatis vero voluptatem. Accusamus, aliquid amet at autem commodi consequuntur culpa deleniti deserunt dolorem dolores eaque eius id illo impedit ipsa ipsam iure libero magni molestias mollitia nam, necessitatibus nemo neque odit placeat quam quasi quibusdam repudiandae sed sequi suscipit tenetur ullam vel velit veniam vero voluptatibus. Accusamus amet animi architecto blanditiis, corporis cumque dicta dolore doloribus ducimus ea et excepturi exercitationem harum hic inventore ipsa laboriosam laborum minima non porro provident, quae reiciendis repellendus sint totam ut veritatis? Alias consequatur cupiditate deleniti perspiciatis provident rerum similique. Alias, aperiam cumque delectus earum error explicabo illo incidunt iste laboriosam magni perspiciatis quam quidem ratione recusandae suscipit ut voluptatibus! Animi consectetur debitis distinctio eligendi id magnam maxime, molestias mollitia, nam, necessitatibus officia perferendis praesentium qui sint soluta totam ullam ut vel. Ab beatae deleniti enim iusto laudantium molestias, nulla officia. Hic, natus, obcaecati. Ad eveniet harum similique vero vitae! Accusantium, adipisci, aperiam at dignissimos doloribus eaque enim exercitationem expedita ipsa neque praesentium provident ratione ullam. Eos est iste maiores. Accusantium adipisci dolorum fugiat omnis possimus provident recusandae repellendus voluptatem! Accusantium, amet architecto aut commodi consectetur deleniti dolores ea error esse facere hic inventore ipsa, ipsum iste itaque iure iusto labore maxime molestiae mollitia natus nesciunt odit officiis omnis perferendis perspiciatis placeat porro provident qui quibusdam repellendus repudiandae rerum sint suscipit ullam veniam veritatis. Animi aspernatur cupiditate deleniti deserunt doloremque doloribus eius error illum incidunt inventore, maiores minima nemo nihil odio officiis perferendis possimus quae sit soluta temporibus. Amet dolore eius, et facere facilis fugit impedit, laboriosam laborum magni minima nam officia optio perspiciatis quam quod rerum ullam vel, veniam voluptates voluptatum! Explicabo iusto quis ut. Asperiores at commodi consectetur consequatur distinctio doloremque dolores est facere fuga laudantium libero minus non, nulla perspiciatis quidem repudiandae tenetur vero?
+    <Head title="Добро пожаловать! Campaign name" path="Заказ" />
+    <div class="order">
+      <div class="order--title">Заказ: Хорошие замки - Самостоятельная заявка </div>
+      <div class="order--fields">
+        <div class="row">
+          <div class="title">Код:</div>
+          <div class="input">
+            <input class="code" type="text">
+          </div>
+        </div>
+        <div class="row">
+          <div class="title">Номер:</div>
+          <div class="input">
+            <input class="number" type="text">
+          </div>
+        </div>
+        <div class="row">
+          <div class="title">Наименование:</div>
+          <div class="input">
+            <input class="name-field" type="text">
+          </div>
+        </div>
+        <div class="row">
+          <div class="title">Срок резервирования:</div>
+          <div class="input">
+            <input class="period" type="text">
+          </div>
+        </div>
+        <div class="row">
+          <div class="title">Надпись на наклейке:</div>
+          <div class="input">
+            <input class="sticker" type="text">
+          </div>
+        </div>
+      </div>
+      <div class="order--btn">
+        <button class="btn black">
+          <img src="../assets/selection.svg" alt="">
+          Подбор товаров
+        </button>
+        <button class="btn black">
+          <img src="../assets/download.svg" alt="">
+          Подбор товаров
+        </button>
+        <button class="btn black">
+          <img src="../assets/upload-order.svg" alt="">
+          Подбор товаров
+        </button>
+      </div>
+      <OrderTable />
+    </div>
   </div>
 </template>
 
 <script>
   import Head from '@/components/Head';
+  import OrderTable from '@/components/Order/orderTable.vue';
 
   export default {
     name: 'Order',
     components: {
-      Head
+      Head,
+      OrderTable
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  ::-webkit-scrollbar {
+  width: 10px;
+  background: transparent;
+}
+::-webkit-scrollbar-track {
+  // -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  // border-radius: 10px;
+}
+::-webkit-scrollbar-thumb {
+  width: 3px;
+  background: transparent;
+  // border-radius: 10px;
 
+  box-shadow: inset 0 0 0 2px #C4C4C4;
+  border: 3px solid transparent;
+}
+.order {
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.06);
+  width: 100%;
+  height: calc(100vh - 270px);
+  padding: 20px;
+  overflow: auto;
+  &--title {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+  &--fields {
+    margin-bottom: 20px;
+    .row {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+      .title {
+        font-weight: 500;
+        font-size: 12px;
+        color: #313131;
+        width: 134px;
+        margin-right: 10px;
+      }
+      .input {
+        input[type="text"] {
+          outline-style: none;
+          padding: 4px 10px;
+          margin: 0;
+          border: 1px solid #DBDBDB;
+          height: 24px;
+          font-size: 12px;
+          color: #313131;
+          &.code {
+            width: 125px;
+          }
+          &.number {
+            width: 150px;
+          }
+          &.name-field {
+            width: 336px;
+          }
+          &.period {
+            width: 78px;
+          }
+          &.sticker {
+            width: 290px;
+          }
+        }
+      }
+    }
+  }
+  &--btn {
+    display: flex;
+    margin-bottom: 16px;
+    .btn {
+      font-size: 13px;
+      height: 28px;
+      margin-right: 10px;
+      img {
+        margin-right: 6px;
+        vertical-align: middle;
+      }
+    }
+  }
+}
 </style>
